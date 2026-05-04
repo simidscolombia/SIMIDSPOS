de650312bbdc2cee2a6ea2c7ed019ffd2f687c24
simidscolombia <simidscolombia@gmail.com>
2026-05-04 08:36:42 -0500

    Initial clean commit: backend/routes/datos.route.js

---
+++ backend/routes/datos.route.js
@@ -0,0 +1,49 @@
+/** =====================================================================
+ *  DATOS ROUTER 
+=========================================================================*/
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+// MIDDLEWARES
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+// CONTROLLERS
+const { getDatos, createDatos, updateDatos } = require('../controllers/datos.controller');
+
+const router = Router();
+
+/** =====================================================================
+ *  GET DATOS
+=========================================================================*/
+router.get('/', getDatos);
+/** =====================================================================
+ *  GET DATOS
+=========================================================================*/
+/** =====================================================================
+ *  POST CREATE DATOS
+=========================================================================*/
+router.post('/', [
+        validarJWT,
+        check('name', 'El nombre es olbigatorio').not().isEmpty(),
+        check('address', 'El nombre es olbigatorio').not().isEmpty(),
+        check('phone', 'El nombre es olbigatorio').not().isEmpty(),
+        check('nit', 'El nombre es olbigatorio').not().isEmpty(),
+        check('tax', 'El nombre es olbigatorio').not().isEmpty(),
+        validarCampos
+    ],
+    createDatos
+);
+/** =====================================================================
+ *  POST CREATE DATOS
+=========================================================================*/
+/** =====================================================================
+ *  PUT DATOS
+=========================================================================*/
+router.put('/:id', validarJWT, updateDatos);
+/** =====================================================================
+ *  PUT DATOS
+=========================================================================*/
+
+// EXPORT
+module.exports = router;
