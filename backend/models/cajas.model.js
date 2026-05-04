d7286cfc1b34d3ebffd64aa3b1d2661e0e4147f9
simidscolombia <simidscolombia@gmail.com>
2026-05-04 08:24:44 -0500

    Initial commit: backend/models/cajas.model.js

---
+++ backend/models/cajas.model.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+// CAJAS MODEL
+
+const CajaSchema = Schema({
+
+    name: {
+        type: String,
+        require: true
+    },
+
+    description: {
+        type: String,
+    },
+
+    turno: {
+        type: Schema.Types.ObjectId,
+        ref: 'Turno'
+    },
+
+    cajero: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    cerrada: {
+        type: Boolean,
+        default: true
+    },
+
+    status: {
+        type: Boolean,
+        default: true
+    },
+
+    fecha: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+CajaSchema.method('toJSON', function() {
+
+    const { __v, _id, ...object } = this.toObject();
+    object.caid = _id;
+    return object;
+
+});
+
+module.exports = model('Caja', CajaSchema);
