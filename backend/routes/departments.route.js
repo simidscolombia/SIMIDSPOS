9640a06b420969d869123c35dba855424447829b
simidscolombia <simidscolombia@gmail.com>
2026-05-04 08:36:43 -0500

    Initial clean commit: backend/routes/departments.route.js

---
+++ backend/routes/departments.route.js
@@ -0,0 +1,61 @@
+/** =====================================================================
+ *  DEPARTMENTS ROUTER
+=========================================================================*/
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+// MIDDLEWARES
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+// CONTROLLER
+const { getDepartments, createDepartment, updateDepartment, deleteDepartment } = require('../controllers/departments.controller');
+
+const router = Router();
+
+/** =====================================================================
+ *  GET DEPARTMENTS
+=========================================================================*/
+router.get('/', getDepartments);
+/** =====================================================================
+ *  GET DEPARTMENTS
+=========================================================================*/
+
+/** =====================================================================
+ *  CREATE DEPARTMENTS
+=========================================================================*/
+router.post('/', [
+        validarJWT,
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    createDepartment
+);
+/** =====================================================================
+ *  CREATE DEPARTMENTS
+=========================================================================*/
+
+/** =====================================================================
+ *  UPDATE DEPARTMENTS
+=========================================================================*/
+router.put('/:id', [
+        validarJWT,
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    updateDepartment
+);
+/** =====================================================================
+ *  UPDATE DEPARTMENTS
+=========================================================================*/
+
+/** =====================================================================
+ *  DELETE DEPARTMENTS
+=========================================================================*/
+router.delete('/:id', validarJWT, deleteDepartment);
+/** =====================================================================
+ *  DELETE DEPARTMENTS
+=========================================================================*/
+
+// EXPORTS
+module.exports = router;
