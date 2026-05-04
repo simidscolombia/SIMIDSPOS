ce3de560a6a2f412f93728efe916f4708f111890
simidscolombia <simidscolombia@gmail.com>
2026-05-04 08:41:08 -0500

    Initial clean commit: frontend/src/app/shared/shared.module.ts

---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+// COMPONENTS
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class SharedModule { }
